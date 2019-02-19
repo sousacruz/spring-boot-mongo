73937aaa901967de16ec9563f3d1b01d5c999def
Herbert <sousacruz@hotmail.com>
2019-02-18 23:45:44 -0300

    Using DTO pattern to retrieve users

--- src/main/java/com/synox/workshopmongo/resources/UserResource.java
+++ src/main/java/com/synox/workshopmongo/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.synox.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.synox.workshopmongo.domain.User;
+import com.synox.workshopmongo.dto.UserDTO;
 import com.synox.workshopmongo.services.UserService;
 
 @RestController
@@ -21,8 +21,9 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(u -> new UserDTO(u)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/com/synox/workshopmongo/services/UserService.java
+++ src/main/java/com/synox/workshopmongo/services/UserService.java
@@ -17,5 +17,4 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-
 }
