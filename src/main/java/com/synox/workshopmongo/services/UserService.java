e10a92ab18d7c7c2e191b5a6d4f0e811dcc65652
Herbert <sousacruz@hotmail.com>
2019-02-19 19:43:30 -0300

    Updating data using HTTP/PUT

--- src/main/java/com/synox/workshopmongo/services/UserService.java
+++ src/main/java/com/synox/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.synox.workshopmongo.domain.User;
+import com.synox.workshopmongo.dto.UserDTO;
 import com.synox.workshopmongo.repositories.UserRepository;
 import com.synox.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,30 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("User not found!"));
+	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User upd) {
+		Optional<User> old = repo.findById(upd.getId());
+		User user = old.get();
+		updateData(user, upd);
+		return repo.save(user);
+	}
+	
+	private void updateData(User user, User upd) {
+		user.setName(upd.getName());
+		user.setEmail(upd.getEmail());
+	}
+
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());	
 	}
 }
