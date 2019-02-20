fa2627a3c25ceb3220a6b4a8794f405fbaf1488f
Herbert <sousacruz@hotmail.com>
2019-02-19 21:07:50 -0300

    Simple query using QueryMethod

--- src/main/java/com/synox/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/synox/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.synox.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.synox.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	public List<Post> findByTitleContainingIgnoreCase(String text);
 }
